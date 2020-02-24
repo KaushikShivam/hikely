0a8bdb1dbc288681bc867ad1df75d43b62c9b8b8
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-24 15:08:51 +0530

    Implement user routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
@@ -92,6 +94,39 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined'
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined'
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined'
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined'
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined'
+  });
+};
+
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -102,6 +137,17 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 const PORT = 3000;
 app.listen(PORT, () => {
