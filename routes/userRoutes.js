6dec1b4c3ec17a208d536ad60b4aaed3018c3382
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-24 15:37:11 +0530

    Refactor user controller and routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const userController = require('./../controllers/userController');
 
 const userRouter = express.Router();
 
 userRouter
   .route('/')
-  .get(getAllUsers)
-  .post(createUser);
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 userRouter
   .route('/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = userRouter;
