fc7ee2b74f2e6f16e96e50fe0a931745e0ed949a
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-26 15:38:10 +0530

    Fetch tours with mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length
-    // data: {
-    //   tours
-    // }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
