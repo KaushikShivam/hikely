df17b5a420af1a1353a32ae50a08c7f00c2cc894
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-04 15:28:02 +0530

    Use document middleware to create slug

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -62,6 +64,13 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+// Document middleware -> Runs before Save and Create command (Won't work on insertMany)
+tourSchema.pre('save', function(next) {
+  // "this" points to the currently processed document
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
