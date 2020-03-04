c4af11f8889a5c2384767ff7d225015bd3e6178c
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-04 23:41:23 +0530

    Add signup auth handler

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppError');
+const User = require('./../models/userModel');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user
+    }
+  });
+});
