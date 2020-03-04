bbb2e636d8bd72fd7179928af489d595f87b5040
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-04 19:27:59 +0530

    Create Error controller

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const AppError = require('./utils/AppError');
+
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
@@ -23,14 +25,7 @@ app.use('/api/v1/users', userRouter);
 
 // Hanlder for all unhandled routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 //global error handling middleware
