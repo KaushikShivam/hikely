763e0b42ba52331e8f19737afe14dc5502b29cea
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-04 19:28:50 +0530

    Export and use error handler

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const AppError = require('./utils/AppError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -29,13 +30,6 @@ app.all('*', (req, res, next) => {
 });
 
 //global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
