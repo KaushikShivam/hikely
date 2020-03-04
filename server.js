8b79778ce985d1585983446c1b1d28ad8d425468
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-04 22:15:06 +0530

    handle unhandled rejections

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ mongoose
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port: ${PORT}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection! Shutting down');
+  server.close(() => {
+    process.exit(1);
+  });
 });
