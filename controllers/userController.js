42ff59651b17f6db5f668823f92724efa5a4c323
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-05 18:19:27 +0530

    Finish updating user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,38 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppError');
+const User = require('./../models/userModel');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1. Create error if user tries to update password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates', 400));
+  }
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user
+    }
+  });
+});
+
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
