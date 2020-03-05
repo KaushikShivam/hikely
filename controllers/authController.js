cefe41f4f54bb4ab28d8021c367063afc364493a
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-05 11:34:56 +0530

    Add safe signuop

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,12 @@ const AppError = require('./../utils/AppError');
 const User = require('./../models/userModel');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
 
   res.status(201).json({
     status: 'success',
