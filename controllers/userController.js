9fc697bc9d046c3665275a0cde585830894c5a21
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-07 23:27:57 +0530

    Create handle factory function to create documents

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,18 +57,6 @@ exports.getUser = (req, res) => {
   });
 };
 
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The route is not yet defined'
-  });
-};
-
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The route is not yet defined'
-  });
-};
-
+exports.createUser = factory.createOne(User);
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
