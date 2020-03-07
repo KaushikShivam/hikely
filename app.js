a29a8192aa334c592698afab3ba4cc66ec1a28ae
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-07 18:39:50 +0530

    Create review router and controller

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -56,6 +57,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Hanlder for all unhandled routes
 app.all('*', (req, res, next) => {
