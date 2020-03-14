43730116b7ba0ba6a52e6113d5b31d2af1e719f8
shivam <shivam.kaushik@outlook.com>
2020-03-14 04:53:01 -0700

    Add cookie parser in auth

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -36,6 +37,7 @@ app.use('/api', limiter);
 app.use(helmet());
 
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 app.use(mongoSanitize());
 
@@ -57,6 +59,7 @@ app.use(
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
+  console.log(req.cookies);
   next();
 });
 
