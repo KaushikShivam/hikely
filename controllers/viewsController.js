43dc6198538e7024fb29374cc0c98ed72f1c1a3a
shivam <shivam.kaushik@outlook.com>
2020-03-14 03:00:42 -0700

    Build tour page part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,7 +11,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const Tour = await Tour.findOne({ slug: req.params.slug }).populate({
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user'
   });
