5e461912a13e56cfcec51f565c350eb728f8ca20
shivam <shivam.kaushik@outlook.com>
2020-03-15 06:46:54 +0530

    Create booking resources

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cookieParser = require('cookie-parser');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const AppError = require('./utils/AppError');
@@ -68,6 +69,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // Hanlder for all unhandled routes
 app.all('*', (req, res, next) => {
